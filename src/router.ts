939f58507d1032234ddab43341d65474c8c1db85
RemiSaurel <r.saurel5@gmail.com>
2024-06-22 09:15:25 +0200

    fix: webHashHistory

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import MainPage from "./pages/MainPage.vue";
 import NotFound from "./pages/NotFound.vue";
 import { articles } from './articles';
@@ -12,7 +12,7 @@ const routes = [
 ];
 
 export const router = createRouter({
-    history: createWebHistory('/studoby-web/'),
+    history: createWebHashHistory('/studoby-web/'),
     routes,
 });
 
