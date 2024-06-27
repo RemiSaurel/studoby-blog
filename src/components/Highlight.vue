52774a4045325f25ba9e0d914103370899221e06
RemiSaurel <r.saurel5@gmail.com>
2024-06-27 17:22:13 +0200

    chore: new blog components + fix

--- src/components/Highlight.vue
+++ src/components/Highlight.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-
+const props = defineProps<{
+  color: 'red' | 'green' | 'orange' | 'blue' | 'purple' | 'yellow'
+}>()
+const colors = {
+  orange: 'bg-orange-1 ',
+  red: 'bg-red-1 ',
+  green: 'bg-green-1',
+  blue: 'bg-cyan-1',
+  purple: 'bg-purple-1',
+  yellow: 'bg-yellow-1',
+}
 </script>
 
 <template>
-  $END$
+  <div class="inline-flex w-fit px-1 rounded" :class="colors[props.color]">
+    <slot></slot>
+  </div>
 </template>
 
 <style scoped>
--- src/components/blog/Image.vue
+++ src/components/blog/Image.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-
+const props = defineProps<{
+  legend: string;
+}>()
 </script>
 
 <template>
-  $END$
+  <div class="flex flex-col items-center justify-center gap-4 bg-white rounded-lg p-4 overflow-auto"
+  >
+    <slot></slot>
+    <div class="text-center italic mt-2 mb-4" v-if="props.legend">
+      {{ props.legend }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
